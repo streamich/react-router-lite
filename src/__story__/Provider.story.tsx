67644796a1575ebf355ebafedfa2c5746df2f0f9
streamich <streamich@gmail.com>
2019-04-09 16:09:23 +0200

    feat: 🎸 add default <Provider> component

--- src/__story__/Provider.story.tsx
+++ src/__story__/Provider.story.tsx
@@ -1,29 +1,23 @@
 import * as React from 'react';
 import {storiesOf} from '@storybook/react';
-import useLocation from 'react-use/lib/useLocation';
-import {Router, Route, Link, go} from '..';
+import {Provider, Route, Link, go} from '..';
 
 const Demo: React.FC<{}> = () => {
-   const {pathname} = useLocation();
-
    return (
-      <Router route={pathname}>
+      <Provider>
          <div>
             <div>
                <button onClick={() => go('page-1')}>Page 1</button>
                <Link to="page-2">Page 2</Link>
             </div>
             <div>
-               Route: {pathname}
-            </div>
-            <div>
                <Route match="/page-1">This is page one</Route>
                <Route match="/page-2">This is page two</Route>
             </div>
          </div>
-      </Router>
+      </Provider>
    );
 };
 
-storiesOf('useLocation', module)
+storiesOf('<Provider>', module)
   .add('Real navigation', () => <Demo />)
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import {createElement as h, FC} from 'react';
+import useLocation from 'react-use/lib/useLocation';
 import create from './create';
-import go from './go';
 
-const {context, Match, Route, Router, useMatch, Link, Switch} = create(go);
+const {context, go, Match, Route, Router, useMatch, Link, Switch} = create();
 
-export {context, go, Match, Route, Router, useMatch, Link, Switch};
+const Provider: FC<{route?: string}> = ({route, children}) =>
+   h(Router, {route: useLocation().pathname || route || '', children});
+
+export {Provider, context, go, Match, Route, Router, useMatch, Link, Switch};
