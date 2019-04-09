a6d889501077f7384dd082724afe3654b0414ad0
streamich <streamich@gmail.com>
2019-04-09 16:11:44 +0200

    test: 💍 add basic server tests

---
+++ src/__tests__/index.server.spec.tsx
@@ -0,0 +1,54 @@
+/**
+ * @jest-environment node
+ */
+
+import * as React from 'react';
+import {renderToStaticMarkup} from 'react-dom/server';
+import {Router, Route} from '..';
+
+describe('route SSR', () => {
+  it('renders without crashing', () => {
+    const html = renderToStaticMarkup(
+      <Router route='/foo/bar'>
+        <Route>
+          <span>baz</span>
+        </Route>
+      </Router>
+    );
+
+    expect(html).toBe('<span>baz</span>');
+  });
+
+  it('renders matching routes', () => {
+    const html = renderToStaticMarkup(
+      <Router route='/foo/bar'>
+        <Route>
+          <span>1</span>
+        </Route>
+        <Route match='/baz'>
+          <span>2</span>
+        </Route>
+        <Route match='/foo'>
+          <span>3</span>
+        </Route>
+        <Route match='/foo/bar'>
+          <span>4</span>
+        </Route>
+      </Router>
+    );
+
+    expect(html).toBe('<span>1</span><span>3</span><span>4</span>');
+  });
+
+  it('matches partial step', () => {
+    const html = renderToStaticMarkup(
+      <Router route='/foo/bar'>
+        <Route match='/foo/b'>
+          <span>1</span>
+        </Route>
+      </Router>
+    );
+
+    expect(html).toBe('<span>1</span>');
+  });
+});
