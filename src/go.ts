8d3a6356922eebdd783b58be04baef9d6b3a71b7
streamich <streamich@gmail.com>
2024-05-18 13:28:57 +0200

    fix: 🐛 remove history access on server

--- src/go.ts
+++ src/go.ts
@@ -8,10 +8,8 @@ export interface IGoParams {
 
 export type Go = (page: string, params?: IGoParams) => void;
 
-const h = history;
-
 export const go: Go = isClient
    ? (page: string, {replace, title, state}: IGoParams = {}) => {
-        replace ? h.replaceState(state, title || '', page) : h.pushState(state, title || '', page);
+        replace ? history.replaceState(state, title || '', page) : history.pushState(state, title || '', page);
      }
    : ((() => {}) as Go);
